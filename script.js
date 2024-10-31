1115769daa639302bbd5707c48b0995faab325a1
Anna Marakasova <annamarakasova2016@gmail.com>
2024-10-31 19:08:40 +0400

    Feat: add correct time display

--- script.js
+++ script.js
@@ -10,30 +10,40 @@ const secondsEl = document.getElementById("seconds");
 const pmAm = document.getElementById("pm-am");
 const themeBtn = document.querySelector(".theme-icons");
 
-// Getting correct day, date, and time
-const currentDate = new Date();
-const day = currentDate.getDay();
-const month = currentDate.getMonth();
-const date = currentDate.getDate();
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
-
-const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-dayEl.innerText = weekdays[day - 1];
-monthEl.innerText = `${months[month]} ${date}`;
-
-hoursEl.innerText = hours > 12 ? hours - 12 : hours;
-pmAm.innerText = hours > 12 ? "PM" : "AM";
-
-minutesEl.innerText = minutes;
-secondsEl.innerText = seconds;
-
 // Change Light or Dark Mode
 themeBtn.addEventListener("click", () => {
   bodyEl.classList.toggle("dark");
   lightThemeIcon.classList.toggle("hidden");
   darkThemeIcon.classList.toggle("hidden");
 });
+
+function updateClock() {
+  // Getting correct day, date, and time
+  const now = new Date();
+  const day = now.getDay();
+  const month = now.getMonth();
+  const date = now.getDate();
+  const hoursExact = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  // Setting the day and the date
+  const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  dayEl.innerText = weekdays[day - 1];
+  monthEl.innerText = `${months[month]} ${date}`;
+
+  // Displaying the time
+  const hours = hoursExact < 12 ? hoursExact : hoursExact - 12;
+  pmAm.innerText = hoursExact > 12 ? "PM" : "AM";
+  hoursEl.innerText = hours >= 10 ? hours : `0${hours}`;
+
+  minutesEl.innerText = minutes >= 10 ? minutes : `0${minutes}`;
+  secondsEl.innerText = seconds >= 10 ? seconds : `0${seconds}`;
+  console.log(seconds);
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
